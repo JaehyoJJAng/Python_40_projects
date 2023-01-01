172ad093f3c765faaf3831e6f7112e17a12d9e20
JaehyoJJang <yshrim12@naver.com>
2023-01-01 16:44:44 +0900

    01 파이썬으로 다른 프로그램 실행하기 - execute_app 함수 구현

--- 22_파이썬으로 다른 프로그램 실행하기/main.py
+++ 22_파이썬으로 다른 프로그램 실행하기/main.py
@@ -1,10 +1,21 @@
 """ 맥북에서는 os.start 명령어 실행 불가함 """
 import os
+import subprocess
+import time
 
 class Util:
     def os_startfile(self, filePath: str) -> None:
         """ os.startfile의 기능을 이용하여 윈도우의 파일 실행하기 """
         os.startfile(filePath)
+
+    def execute_app(self) -> None:
+        """ 윈도우의 응용 프로그램 실행하고 종료 """
+        memo_process = subprocess.Popen('notepad')
+        time.sleep(5)
+
+        if memo_process.poll() == None:
+            print('메모장을 종료합니다')
+            memo_process.kill()
 
 def main():
     # Util Instance
@@ -12,6 +23,9 @@ def main():
 
     # 윈도우 파일 실행하기
     util.os_startfile(filePath='txt/test.txt')
+
+    # 윈도우 응용 프로그램 실행하기
+    util.execute_app()
 
 if __name__ == '__main__':
     main()
